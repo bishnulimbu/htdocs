9ae242d2771f202d04e7613c74324104bf2abce2
bishnulimbu <subashlimbu987654321@gmail.com>
2024-03-01 10:07:48 +0545

    dfd

---
+++ array1.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<div class= "container">
+<?php
+$colors = array("red", "blue", "green");
+forEach($colors as $data){
+  echo $data;
+  echo "<br>";
+}
+?>
+</div>
+  </body>
+  </html>
